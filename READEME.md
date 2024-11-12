e82bf08e3b396949ea8bc2589da0f0493f7b4971
Mujeeb <itsmdmujeeb@gmail.com>
2024-11-13 00:09:41 +0530

    global error handling

---
+++ READEME.md
@@ -0,0 +1,17 @@
+# Global Error Handling
+
+. Two types of Errors
+
+    1. Operational Errors
+        . Operational errors are the problems that we can predict that will happen at some point in future. We need to handle them advanced
+            
+            a. User trying to access an invalid roue
+            b. inputting invalid data
+            c. application failed to connect to server
+            d. request timeout, etc. 
+
+    2. Programming Errors
+        . Programmding errors are simply bugs that we programmers, by mistake, introduces them in code
+            a. trying to read property of an undefined variable
+            b. using await without async
+            c. passing a number where an object is expected, etc.
--- app.js
+++ app.js
@@ -3,10 +3,40 @@ import router from './routes/index.js';
 
 const app = Express();
 
-app.use(Express.json({ limit: "16kb"}));
-app.use(Express.urlencoded({ extended: true, limit: "16kb"}));
+app.use(Express.json({ limit: "16kb" }));
+app.use(Express.urlencoded({ extended: true, limit: "16kb" }));
 
 // Routes
 app.use("/api", router);
+
+// Default Route Error
+app.all('*', (req, res, next) => {
+    // return res.status(404).json({
+    //     status: 'failed',
+    //     message: `Can't find ${req.originalUrl} on the server`
+    // });
+
+    // error object
+    const error = new Error(`Can't find ${req.originalUrl} on the server`);
+    error.status = 'fail';
+    error.statusCode = 404;
+
+    next(error); // when we pass an argument to next function no matter what that argument is express will automatically know there was error, in that case express will skip all other middleware functions which is currently present in middleware stack and it will directly call the global middleware error handling function 
+});
+
+
+// Global Error Handling middleware
+// when we specify these 4 parameters in a middleware express will automatically recognize it as error handling middleware, therefor express will only call this middleware when there is an error
+
+// # Whenever we want to call this middleware from any part of the application code first we need to create an error object
+app.use((err, req, res, next) => {
+    err.statusCode = err.statusCode || 500; // if this error has status code send that statu code if not send 500 status code
+    err.status = err.status || 'error';
+
+    res.status(err.statusCode).json({
+        status: err.statusCode,
+        message: err.message,
+    })
+});
 
 export { app };
